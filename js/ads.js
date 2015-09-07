6e34e94688d10d08add7e30390ebce393a925583
crawlingout <crawlingout@zoho.com>
2015-09-07 16:08:55 +0200

    Affiliate links from Amazon

--- js/ads.js
+++ js/ads.js
@@ -1,14 +1,19 @@
 var ads = [
 // end date 12-31-2015
-'<a href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=353984.231&type=4&subid=0"><img alt="Travelation Brand Logo" border="0" src="http://www.travelation.com/Sites/3/Linkshare/TLLogo/TLLogo_250X100.jpg" /></a><img border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=353984.231&type=4&subid=0" /><br><a id="ad_txt" href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=353984.142&type=3&subid=0" >Fastest way to compare and book over 500 special airlines flights! Get extra $15 off.</a><img border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=353984.142&type=3&subid=0" />',
+//'<a href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=353984.231&type=4&subid=0"><img alt="Travelation Brand Logo" border="0" src="http://www.travelation.com/Sites/3/Linkshare/TLLogo/TLLogo_250X100.jpg" /></a><img border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=353984.231&type=4&subid=0" /><br><a id="ad_txt" href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=353984.142&type=3&subid=0" ><strong>Fastest way to compare and book flights!</strong></a><img border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=353984.142&type=3&subid=0" />',
 
 // end date 09-30-2015
-'<a href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=222795.10000254&type=4&subid=0&LSNSUBSITE=LSNSUBSITE"><img style="margin: 10px 0 5px;" alt="ONETRAVEL.COM" border="0" src="http://www.onetravel.com/travel/affiliate/logo/images/200x50.gif"></a><img border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=222795.10000254&type=4&subid=0"><br><a id="ad_txt" href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=222795.10000185&type=3&subid=0&LSNSUBSITE=LSNSUBSITE" >Fly and Stay Cheap with OneTravel.com. Low Fare Promise. Book Now!</a><img border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=222795.10000185&type=3&subid=0" >'
-];
+//'<a href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=222795.10000254&type=4&subid=0&LSNSUBSITE=LSNSUBSITE"><img style="margin: 10px 0 5px;" alt="ONETRAVEL.COM" border="0" src="http://www.onetravel.com/travel/affiliate/logo/images/200x50.gif"></a><img border="0" width="1" height="1" src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=222795.10000254&type=4&subid=0"><br><a id="ad_txt" href="http://click.linksynergy.com/fs-bin/click?id=hMT*QUxbGMY&offerid=222795.10000185&type=3&subid=0&LSNSUBSITE=LSNSUBSITE" ><strong>Fly and stay cheap with onetravel.com.</strong></a><img border=0 width=1 height=1 src="http://ad.linksynergy.com/fs-bin/show?id=hMT*QUxbGMY&bids=222795.10000185&type=3&subid=0" >',
 
-var number_of_ads = ads.length;
-var ad = getRandomInt(0, number_of_ads-1);
-var insert_ad = document.getElementById('insert_ad');
+// Canon EOS 5D Mark III at amazon.com
+'<a href="http://www.amazon.com/gp/product/B007FGZ1V0/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B007FGZ1V0&linkCode=as2&tag=simplyeasy-20&linkId=O4CR2K4CJJHRQXDS"><img style="padding-left: 15px;width: 130px;" border="0" src="http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B007FGZ1V0&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=simplyeasy-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=simplyeasy-20&l=as2&o=1&a=B007FGZ1V0" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /><br><br><span style="font-size: 14px;font-weight: bold;line-height: 20px;">Buy <a href="http://www.amazon.com/gp/product/B007FGZ1V0/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B007FGZ1V0&linkCode=as2&tag=simplyeasy-20&linkId=CEAFQPH7SHKGZQGL">Canon EOS 5D Mark III</a></span><img src="http://ir-na.amazon-adsystem.com/e/ir?t=simplyeasy-20&l=as2&o=1&a=B007FGZ1V0" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
+
+// Sony DSCRX10/B Cybershot at amazon.com
+'<a href="http://www.amazon.com/gp/product/B00FRHTSMW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00FRHTSMW&linkCode=as2&tag=simplyeasy-20&linkId=TR5QBBMYECIR2AVS"><img style="padding-left: 10px;width: 180px;" border="0" src="http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00FRHTSMW&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=simplyeasy-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=simplyeasy-20&l=as2&o=1&a=B00FRHTSMW" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /><br><br><span style="font-size: 14px;font-weight: bold;line-height: 20px;">Buy <a href="http://www.amazon.com/gp/product/B00FRHTSMW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00FRHTSMW&linkCode=as2&tag=simplyeasy-20&linkId=IKLEGUAKVHPWSLPV">Sony DSCRX10/B Cybershot</a></span><img src="http://ir-na.amazon-adsystem.com/e/ir?t=simplyeasy-20&l=as2&o=1&a=B00FRHTSMW" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
+
+// Nikon D4S at amazon.com
+'<a href="http://www.amazon.com/gp/product/B00IMOB5SW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00IMOB5SW&linkCode=as2&tag=simplyeasy-20&linkId=244P3Z2RN4YEUFRE"><img style="padding-left: 30px;width: 160px;" border="0" src="http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00IMOB5SW&Format=_SL250_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=simplyeasy-20" ></a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=simplyeasy-20&l=as2&o=1&a=B00IMOB5SW" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" /><br><br><span style="font-size: 13px;font-weight: bold;line-height: 20px;">Buy <a href="http://www.amazon.com/gp/product/B00IMOB5SW/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00IMOB5SW&linkCode=as2&tag=simplyeasy-20&linkId=ZAMPCMYCOAJEEMNA">Nikon D4S (Full 1080p HD Video)</a></span><img src="http://ir-na.amazon-adsystem.com/e/ir?t=simplyeasy-20&l=as2&o=1&a=B00IMOB5SW" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
+];
 
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
@@ -17,5 +22,9 @@ var insert_ad = document.getElementById('insert_ad');
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+var number_of_ads = ads.length;
+var ad = getRandomInt(0, number_of_ads-1);
+var insert_ad = document.getElementById('insert_ad');
 
 insert_ad.innerHTML = ads[ad];
